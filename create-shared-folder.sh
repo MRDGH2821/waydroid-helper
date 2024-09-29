7cdf03da9c57de0bd8c6379b4803362ae7d21572
MRDGH2821 <ask.mrdgh2821@outlook.com>
2024-09-29 22:37:39 +0530

    chore: notify when folder is created

--- create-shared-folder.sh
+++ create-shared-folder.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
-source="$HOME/Waydroid-Shares"
+source="$HOME/Waydroid-Shares/"
 target="Waydroid-Shares"
-absolute_target="$HOME/.local/share/waydroid/data/media/0/$target"
+absolute_target="$HOME/.local/share/waydroid/data/media/0/$target/"
 
 mkdir -p "$source"
 
 sudo mkdir -p "$absolute_target"
 
 sudo mount --bind "$source" "$absolute_target"
+
+echo "Shared folder created at $absolute_target"
+echo "Add your files at $source"

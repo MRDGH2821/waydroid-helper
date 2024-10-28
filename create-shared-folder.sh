8a81d39c091b84732888fa553d6a980a58b97c3d
MRDGH2821 <ask.mrdgh2821@outlook.com>
2024-10-28 22:02:28 +0530

    fix: :rotating_light: fix linter errors

--- create-shared-folder.sh
+++ create-shared-folder.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-source="$HOME/Waydroid-Shares/"
+source="${HOME}/Waydroid-Shares/"
 target="Waydroid-Shares"
-absolute_target="$HOME/.local/share/waydroid/data/media/0/$target/"
+absolute_target="${HOME}/.local/share/waydroid/data/media/0/${target}/"
 
-mkdir -p "$source"
+mkdir -p "${source}"
 
-sudo mkdir -p "$absolute_target"
+sudo mkdir -p "${absolute_target}"
 
-sudo mount --bind "$source" "$absolute_target"
+sudo mount --bind "${source}" "${absolute_target}"
 
-echo "Shared folder created at $absolute_target"
-echo "Add your files at $source"
+echo "Shared folder created at ${absolute_target}"
+echo "Add your files at ${source}"
--- internet-fixers/provide-internet-via-interface.sh
+++ internet-fixers/provide-internet-via-interface.sh
@@ -6,9 +6,9 @@
 interface="wlp0s20f3"
 
 # Use iptables
-sudo iptables -A FORWARD -i waydroid0 -o $interface -j ACCEPT
-sudo iptables -A FORWARD -i $interface -o waydroid0 -j ACCEPT
-sudo iptables -t nat -A POSTROUTING -o $interface -j MASQUERADE
+sudo iptables -A FORWARD -i waydroid0 -o "${interface}" -j ACCEPT
+sudo iptables -A FORWARD -i "${interface}" -o waydroid0 -j ACCEPT
+sudo iptables -t nat -A POSTROUTING -o "${interface}" -j MASQUERADE
 
 # Use nftables
 sudo systemctl start nftables
@@ -25,11 +25,11 @@ sudo nft add chain ip filter forward "{ type filter hook forward priority 0 ; po
 sudo nft add chain ip nat postrouting "{ type nat hook postrouting priority 100 ; }"
 
 # Add equivalent forwarding rules
-sudo nft add rule ip filter forward iif "waydroid0" oif $interface accept
-sudo nft add rule ip filter forward iif $interface oif "waydroid0" accept
+sudo nft add rule ip filter forward iif "waydroid0" oif "${interface}" accept
+sudo nft add rule ip filter forward iif "${interface}" oif "waydroid0" accept
 
 # Add equivalent NAT rules
-sudo nft add rule ip nat postrouting oif $interface masquerade
+sudo nft add rule ip nat postrouting oif "${interface}" masquerade
 
 # Save config
 sudo nft list ruleset | sudo tee /etc/nftables.conf
